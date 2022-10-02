d21ab90daf154fc7543b95701b300afb6bee0697
katsura46 <101129597+katsura46@users.noreply.github.com>
2022-10-03 02:46:15 +0900

    Java score

---
+++ FreePractice35.java
@@ -0,0 +1,15 @@
+
+public class FreePractice35 {
+	public static void main(String[] args) {
+		int bigNum = 0;
+		for(int i = 0; i < 10; i++ ) {
+			int num = Integer.parseInt(args[i]);
+			if( num > bigNum) {
+				bigNum = num;
+			}
+		}
+		
+		System.out.println(args.length + "回入力されたの数字の内最大の値は" + bigNum + "です。");
+	}
+
+}
